06b5927d2d9c7636c2c397631c10061272e00579
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-02-13 18:23:54 +0300

    Implement insertion

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ import           Control.Monad    (mapM)
 import           Control.Monad.ST
 import           Data.Array.ST
 import           Data.Bits        (Bits (..))
+import           Data.Bool        (bool)
 import           Data.Int         (Int16, Int64)
 import           Data.List        (intersperse)
 import           Data.Maybe       (isNothing)
@@ -31,17 +32,24 @@ instance BRep Int64 where
 instance BRep Int16 where
     totalBits = 16
 
+-- for testing
+newtype Int4 = Int4 Int16 deriving (Eq,Ord,Real,Num,Enum,Integral,Bits,Ix)
+instance Show Int4 where
+    show (Int4 x) = show x
+instance BRep Int4 where
+    totalBits = 4
+
 data VEB s i
     = VNode { vChildren :: STArray s i (VEB s i)
             , vAux      :: STRef s (VEB s i)
-            , vMinMax   :: Maybe (STRef s (i, i)) }
-    | VLeaf { vMinMax :: Maybe (STRef s (i, i)) }
+            , vMinMax   :: STRef s (Maybe (i, i)) }
+    | VLeaf { vMinMax :: STRef s (Maybe (i, i)) }
 
-isEmpty :: (BRep i) => VEB s i -> Bool
-isEmpty v = isNothing $ vMinMax v
+isEmpty :: (BRep i) => VEB s i -> ST s Bool
+isEmpty v = isNothing <$> readSTRef (vMinMax v)
 
 getMinMax :: ((i,i) -> i) -> VEB s i -> ST s (Maybe i)
-getMinMax g = maybe (pure Nothing) (fmap (Just . g) . readSTRef) . vMinMax
+getMinMax g v = fmap g <$> readSTRef (vMinMax v)
 
 getMin, getMax :: VEB s i -> ST s (Maybe i)
 getMin = getMinMax fst
@@ -57,12 +65,12 @@ member = memberGo (totalBits @i)
                 getChild v (takeHigh k e) >>= \case
                     Nothing -> pure False -- child doesn't even exist
                     Just vc -> memberGo (k `div` 2) vc (takeLow k e)
-            checkChildren _            = error "member: doesn't make sense"
+            checkChildren _            = pure False
 
-        maybe (pure False) (\mm -> readSTRef mm >>= \(mn,mx) ->
-                               if e == mn || e == mx
-                               then pure True
-                               else checkChildren v) $ vMinMax v
+        mm <- readSTRef (vMinMax v)
+        maybe (pure False) (\(mn,mx) -> if e == mn || e == mx
+                                        then pure True
+                                        else checkChildren v) mm
 
 -- inline
 getChild :: (BRep i) => VEB s i -> i -> ST s (Maybe (VEB s i))
@@ -76,57 +84,80 @@ getChild VNode{..} j = do
 printVeb :: forall i. (BRep i, Show i) => VEB RealWorld i -> IO ()
 printVeb v0 = putStrLn =<< stToIO (printGo (totalBits @i) v0)
   where
-    printMinMax = maybe (pure "mm0") (\s -> show <$> readSTRef s)
+    printMinMax s = maybe "\"mm0\"" (show . show) <$> readSTRef s
     printGo :: Int -> VEB RealWorld i -> ST RealWorld String
     printGo _ VLeaf {..} = do
         e <- printMinMax vMinMax
-        pure $ concat [ "{ leaf ", e, " }" ]
+        pure $ concat [ "{ \"leaf\": ", e, " }" ]
     printGo k VNode {..} = do
         let printChild j = do
                 b <- readSTRef vAux >>= \aux -> member aux j
                 if b
                     then printGo (k `div` 2) =<< readArray vChildren j
-                    else pure "N"
+                    else pure "null"
         e1 <- concat . intersperse ", " <$> mapM printChild [0..(fromIntegral k)-1]
         (e2 :: String) <- printGo (k `div` 2) =<< readSTRef vAux
         e3 <- printMinMax vMinMax
-        pure $ concat ["{ children: (", e1, "), aux: (", e2, "), mm: ", e3, " }"]
+        pure $ concat ["{ \"children\": [", e1, "], \"aux\": ", e2, ", \"mm\": ", e3, "}"]
 
 newVEB :: forall i s. BRep i => ST s (VEB s i)
 newVEB = newVEBGo $ totalBits @i
-  where
-    newVEBGo 1 = pure $ VLeaf Nothing
-    newVEBGo n = do
-        vChildren <- newArray_ (0, fromIntegral n)
-        vAux <- newSTRef =<< newVEBGo (n `div` 2)
-        let vMinMax = Nothing
-        pure $ VNode {..}
 
-{-
-insert :: forall a s. (BRep a) => VEB s a -> a -> ST s ()
-insert t0 e0 = insertDo (totalBits @a) t0 e0
-  where
-    insertDo :: Integer -> VEB a -> a -> IO ()
-    insertDo s t e
-      | isEmpty t = writeIORef (vMinMax t) (e,e)
-      | otherwise = do
-            (tmin,tmax) <- readIORef $ vMinMax t
-            if tmin == tmax
-            then writeIORef (vMinMax t) $ bool (tmin,e) (e,tmax) (tmin < e)
-            else do let newmin = bool tmin e (tmin > e)
-                    let newmax = bool tmax e (tmax < e)
-                    writeIORef (vMinMax t) (newmin,newmax)
-                    let ehigh = takeHigh @a s
-                    let elow = takeLow @a s
-                    let s' = s `div` 2
-                    child <- readIORef (vChildren t !! (fromIntegral ehigh))
-                    when (isEmpty child) $
-                        readIORef (vAux t) >>= (\aux -> insertDo s' aux ehigh)
-                    insertDo (s `div` 2) child elow
+newVEBGo :: BRep i => Int -> ST s (VEB s i)
+newVEBGo 1 = VLeaf <$> newSTRef Nothing
+newVEBGo n = do
+    vChildren <- newArray_ (0, fromIntegral n)
+    vAux <- newSTRef =<< newVEBGo (n `div` 2)
+    vMinMax <- newSTRef Nothing
+    pure $ VNode {..}
 
--}
+insert :: forall i s. (BRep i) => VEB s i -> i -> ST s ()
+insert = insertDo (totalBits @i)
+  where
+    insertDo :: Int -> VEB s i -> i -> ST s ()
+    insertDo k v e = do
+        let onEmptyMM = writeSTRef (vMinMax v) (Just (e,e))
+            onFullMM (tmin,tmax) = do
+                let newmin = bool tmin e (tmin > e)
+                let newmax = bool tmax e (tmax < e)
+                writeSTRef (vMinMax v) $ Just (newmin,newmax)
+                let ehigh = takeHigh k e
+                let elow = takeLow k e
+                let k' = k `div` 2
+                case v of
+                    VLeaf{} -> pure ()
+                    VNode{..} -> do
+                        aux <- readSTRef vAux
+                        childExists <- member aux ehigh
+                        if childExists
+                           then readArray vChildren ehigh >>= \v' -> insertDo k' v' elow
+                           else do
+                               insertDo k' aux ehigh
+                               newTree <- newVEBGo k'
+                               insertDo k' newTree elow
+                               writeArray vChildren ehigh newTree
+
+        maybe onEmptyMM onFullMM =<< readSTRef (vMinMax v)
 
 main :: IO ()
 main = do
-    !x <- stToIO (newVEB @Int16)
-    printVeb x
+    v <- stToIO $ do
+        v <- newVEB @Int4
+        mapM_ (insert v) [0, 1, 2, 3, 5, 14, 15]
+        pure v
+    printVeb v
+
+{-
+{ "children": [ { "children": [null, { "leaf": "(0,1)" }]
+                , "aux": { "leaf": "(1,1)" }
+                , "mm": {"(1,3)"} }
+              , { "children": [null, null]
+                , "aux": { "leaf": "mm0" }
+                , "mm": {"(1,1)"} }
+              , null
+              , { "children": [ null, { "leaf": "(1,1)" }]
+                , "aux": { "leaf": "(1,1)" }
+                , "mm": {"(2,3)"} }
+              ]
+, "aux": { "children": [{ "leaf": "(1,1)" }, { "leaf": "(1,1)" }], "aux": { "leaf": "(0,1)" }, "mm": {"(0,3)"} }, "mm": {"(0,15)"} }
+-}
