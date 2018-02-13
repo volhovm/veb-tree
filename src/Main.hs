9c98e8fb6c9b0ae1045577284c04a0acfa2f8d92
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-02-13 20:28:45 +0300

    Fix for Int4

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,18 @@
 
 module Main where
 
-import           Control.Monad    (forM, forM_, mapM, replicateM)
+import           Control.Monad    (forM, forM_, mapM, replicateM, replicateM_, unless,
+                                   when)
 import           Control.Monad.ST
 import           Data.Array.ST
 import           Data.Bits        (Bits (..))
 import           Data.Bool        (bool)
-import           Data.Int         (Int16, Int64)
-import           Data.List        (intersperse, nub)
+import           Data.Int         (Int16, Int64, Int8)
+import           Data.List        (intersperse, nub, sort)
 import           Data.Maybe       (isNothing)
 import           Data.STRef
+import           Data.Word
+import           Debug.Trace      (trace, traceShow, traceShowM)
 import           System.Random
 
 -- could be Word8 instead of Int here, it's used for depth only
@@ -32,6 +35,12 @@ instance BRep Int64 where
 
 instance BRep Int16 where
     totalBits = 16
+
+instance BRep Int8 where
+    totalBits = 8
+
+instance BRep Word8 where
+    totalBits = 8
 
 -- for testing
 newtype Int4 = Int4 Int16 deriving (Eq,Ord,Real,Num,Enum,Integral,Bits,Ix)
@@ -59,25 +68,29 @@ getMax = getMinMax snd
 
 member :: forall s i. (BRep i) => VEB s i -> i -> ST s Bool
 member = memberGo (totalBits @i)
-  where
-    memberGo :: Int -> VEB s i -> i -> ST s Bool
-    memberGo k v e = do
-        let checkChildren (VNode {..}) =
-                getChild v (takeHigh k e) >>= \case
-                    Nothing -> pure False -- child doesn't even exist
-                    Just vc -> memberGo (k `div` 2) vc (takeLow k e)
-            checkChildren _            = pure False
 
-        mm <- readSTRef (vMinMax v)
-        maybe (pure False) (\(mn,mx) -> if e == mn || e == mx
-                                        then pure True
-                                        else checkChildren v) mm
+memberGo :: forall s i. (BRep i) => Int -> VEB s i -> i -> ST s Bool
+memberGo 0 _ _ = error "memberGo 0"
+memberGo k v e = do
+    let checkChildren (VNode {..}) = do
+            aux <- readSTRef vAux
+            let h = takeHigh k e
+            b <- memberGo (k `div` 2) aux h
+            if b then do vc <- readArray vChildren h
+                         memberGo (k `div` 2) vc (takeLow k e)
+                 else pure False -- child doesn't even exist
+        checkChildren _            = pure False
+
+    mm <- readSTRef (vMinMax v)
+    maybe (pure False) (\(mn,mx) -> if e == mn || e == mx
+                                    then pure True
+                                    else checkChildren v) mm
 
 -- inline
-getChild :: (BRep i) => VEB s i -> i -> ST s (Maybe (VEB s i))
-getChild VLeaf{} _ = error "getChild leaf"
-getChild VNode{..} j = do
-    b <- readSTRef vAux >>= \aux -> member aux j
+getChild :: (BRep i) => Int -> VEB s i -> i -> ST s (Maybe (VEB s i))
+getChild _ VLeaf{} _ = error "getChild leaf"
+getChild k VNode{..} j = do
+    b <- readSTRef vAux >>= \aux -> memberGo (k `div` 2) aux j
     if b
         then Just <$> readArray vChildren j
         else pure Nothing
@@ -92,19 +105,20 @@ printVeb v0 = putStrLn =<< stToIO (printGo (totalBits @i) v0)
         pure $ concat [ "{ \"leaf\": ", e, " }" ]
     printGo k VNode {..} = do
         let printChild j = do
-                b <- readSTRef vAux >>= \aux -> member aux j
+                b <- readSTRef vAux >>= \aux -> memberGo (k `div` 2) aux j
                 if b
                     then printGo (k `div` 2) =<< readArray vChildren j
                     else pure "null"
         e1 <- concat . intersperse ", " <$> mapM printChild [0..(fromIntegral k)-1]
-        --(e2 :: String) <- printGo (k `div` 2) =<< readSTRef vAux
+        (e2 :: String) <- printGo (k `div` 2) =<< readSTRef vAux
         e3 <- printMinMax vMinMax
-        pure $ concat ["{ \"children\": [", e1, "], \"mm\": ", e3, "}"]
+        pure $ concat ["{ \"children\": [", e1, "], \"aux\": ", e2 ,", \"mm\": ", e3, "}"]
 
 newVEB :: forall i s. BRep i => ST s (VEB s i)
 newVEB = newVEBGo $ totalBits @i
 
 newVEBGo :: BRep i => Int -> ST s (VEB s i)
+newVEBGo i | i <= 0 = error "newVEBGo"
 newVEBGo 1 = VLeaf <$> newSTRef Nothing
 newVEBGo n = do
     vChildren <- newArray_ (0, fromIntegral n)
@@ -112,15 +126,19 @@ newVEBGo n = do
     vMinMax <- newSTRef Nothing
     pure $ VNode {..}
 
-insert :: forall i s. (BRep i) => VEB s i -> i -> ST s ()
-insert = insertDo (totalBits @i)
+insert :: forall i s. (BRep i, Show i) => VEB s i -> i -> ST s ()
+insert v00 e00 = insertDo (totalBits @i) v00 e00
   where
     insertDo :: Int -> VEB s i -> i -> ST s ()
-    insertDo k v e = do
-        let onEmptyMM = writeSTRef (vMinMax v) (Just (e,e))
+    insertDo 0 _v _e0 = error "insertDo 0"
+    insertDo k v e0 = do
+        let onEmptyMM = writeSTRef (vMinMax v) (Just (e0,e0))
+            onFullMM (tmin,tmax) | e0 == tmin || e0 == tmax = pure ()
+            onFullMM (tmin,tmax) | tmin == tmax = do
+                let [newmin,newmax] = sort [tmin, e0]
+                writeSTRef (vMinMax v) $ Just (newmin,newmax)
             onFullMM (tmin,tmax) = do
-                let newmin = bool tmin e (tmin > e)
-                let newmax = bool tmax e (tmax < e)
+                let [newmin,e,newmax] = sort [tmin, tmax, e0]
                 writeSTRef (vMinMax v) $ Just (newmin,newmax)
                 let ehigh = takeHigh k e
                 let elow = takeLow k e
@@ -129,7 +147,7 @@ insert = insertDo (totalBits @i)
                     VLeaf{} -> pure ()
                     VNode{..} -> do
                         aux <- readSTRef vAux
-                        childExists <- member aux ehigh
+                        childExists <- memberGo k' aux ehigh
                         if childExists
                            then readArray vChildren ehigh >>= \v' -> insertDo k' v' elow
                            else do
@@ -140,30 +158,110 @@ insert = insertDo (totalBits @i)
 
         maybe onEmptyMM onFullMM =<< readSTRef (vMinMax v)
 
+----------------------------------------------------------------------------
+-- Tests
+----------------------------------------------------------------------------
+
 testInt4 :: IO ()
-testInt4 = do
+testInt4 = replicateM_ 10000 $ do
+    (iter :: Int) <- randomRIO (0, 20)
+    toInsert <- nub . map Int4 <$> replicateM iter (randomRIO (0,15))
     v <- stToIO $ do
         v <- newVEB @Int4
-        mapM_ (insert v) [0, 1, 2, 3, 5, 14, 15]
+        mapM_ (insert v) toInsert
+        pure v
+    forM_ [0..15] $ \i -> do
+        m <- stToIO (member v i)
+        when (m && (not $ i `elem` toInsert)) $ do
+            print $ sort $ toInsert
+            print i
+            error "A"
+        when (not m && (i `elem` toInsert)) $ do
+            print $ sort $ toInsert
+            print i
+            printVeb v
+            error "B"
+
+testBug = do
+    let toInsert = [1,5,10,14,15]
+    v <- stToIO $ do
+        v <- newVEB @Int4
+        mapM_ (insert v) toInsert
         pure v
     printVeb v
     forM_ [0..15] $ \i -> do
-        putStr (show i ++ ": ")
         print =<< stToIO (member v i)
 
+
 -- debug this
-testInt16 :: IO ()
-testInt16 = do
-    (iter :: Int) <- randomRIO (200, 400)
-    vals <- nub <$> replicateM iter (randomIO @Int16)
-    putStrLn "kek"
+testRandom :: forall i. (BRep i, Random i, Show i, Bounded i) => IO ()
+testRandom = do
+    (iter :: Int) <- randomRIO (0, 400)
+    vals <- nub <$> replicateM iter (randomRIO (0, maxBound @i))
+    putStrLn "Generated, inserting"
     v <- stToIO $ do
-        v <- newVEB @Int16
+        v <- newVEB @i
         mapM_ (insert v) vals
         pure v
-    putStrLn "mda"
-    r <- stToIO $ fmap and $ forM [0..maxBound @Int16] $ member v
+    putStrLn "Inserted, checking"
+    r <- stToIO $ fmap and $ forM [0..maxBound @i] $ member v
     print r
 
 main :: IO ()
-main = testInt16
+main = testRandom @Word8
+
+{-
+{ "children": [ { "children": [null, null], "mm": "(3,3)"}
+              , { "children": [null, null], "mm": "(0,0)"}
+              , null, null]
+, "mm": "(0,7)"}
+
+
+{ "children": [ { "children": [null, { "leaf": "(0,0)" }]
+                , "aux": { "leaf": "(1,1)" }
+                , "mm": "(1,3)"}
+              , { "children": [null, null]
+                , "aux": { "leaf": "mm0" }
+                , "mm": "(1,1)"}
+              , null
+              , { "children": [null, null]
+                , "aux": { "leaf": "mm0" }
+                , "mm": "(2,2)"}
+              ]
+, "aux": { "children": [{ "leaf": "(1,1)" }, null]
+         , "aux": { "leaf": "(0,0)" }
+         , "mm": "(0,3)"}
+, "mm": "(0,15)"}
+
+
+Doesn't have 10 but must
+[1,5,10,14,15]
+10
+
+{ "children": [ null
+              , { "children": [null, null]
+                , "aux": { "leaf": "mm0" }
+                , "mm": "(1,1)"}
+              , null
+              , { "children": [null, null]
+                , "aux": { "leaf": "mm0" }
+                , "mm": "(2,2)"}
+              ]
+, "aux": { "children": [null, { "leaf": "(0,0)" }]
+         , "aux": { "leaf": "(1,1)" }
+         , "mm": "(1,3)"}
+, "mm": "(1,15)"}
+
+POSTFIX:
+
+{ "children": [ null
+              , { "children": [null, null], "aux": { "leaf": "mm0" }, "mm": "(1,1)"}
+              , { "children": [null, null], "aux": { "leaf": "mm0" }, "mm": "(2,2)"}
+              , { "children": [null, null], "aux": { "leaf": "mm0" }, "mm": "(2,2)"}]
+, "aux": { "children": [null, { "leaf": "(0,0)" }]
+         , "aux": { "leaf": "(1,1)" }
+         , "mm": "(1,3)"}
+, "mm": "(1,15)"
+}
+
+-}
